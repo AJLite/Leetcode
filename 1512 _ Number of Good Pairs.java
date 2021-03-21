c736c4add794d06258d093ea5f6aa472d2684dbb
AJLite <78590241+AJLite@users.noreply.github.com>
2021-03-21 18:30:38 +0200

    Update 1512 _ Number of Good Pairs.java

--- 1512 _ Number of Good Pairs.java
+++ 1512 _ Number of Good Pairs.java
@@ -1,10 +1,17 @@
+//1512. Number of Good Pairs
 class Solution {
     public int numIdenticalPairs(int[] nums) {
+        
         int cnt = 0;   
-        Map<Integer,Integer> numMap =  new HashMap<>();        
+        Map<Integer,Integer> numMap =  new HashMap<>();  
+        
         for(int num: nums){
+            
+            //each previous appearing of the number meaning a good pair
             int numprevCount = numMap.getOrDefault(num, 0);
             cnt += numprevCount;
+            
+            //update the appearing of number
             numMap.put(num, ++numprevCount);
         }        
         return cnt;
